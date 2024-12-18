dc46d259ec7678be047dc8aa39990e7d00b5ca63
Syliverius Syliakus Aporinary <syliveriusakus@gmail.com>
2024-12-18 20:59:33 +0300

    setting navigation

---
+++ app/Views/templates/footer.php
@@ -0,0 +1,162 @@
+<!-- Footer Start -->
+        <div class="container-fluid footer py-5 wow fadeIn" data-wow-delay="0.2s">
+            <div class="container py-5 border-start-0 border-end-0" style="border: 1px solid; border-color: rgb(255, 255, 255, 0.08);">
+                <div class="row g-5">
+                    <div class="col-md-6 col-lg-6 col-xl-4">
+                        <div class="footer-item">
+                            <a href="index.html" class="p-0">
+                                <h4 class="text-white"><i class="fas fa-search-dollar me-3"></i>E&R Enterprise</h4>
+                                <!-- <img src="img/logo.png" alt="Logo"> -->
+                            </a>
+                            <p class="mb-4">Connect you to global style</p>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-6 col-xl-2">
+                        <div class="footer-item">
+                            <h4 class="text-white mb-4">Quick Links</h4>
+                            <a href="#"><i class="fas fa-angle-right me-2"></i> About Us</a>
+                            <a href="#"><i class="fas fa-angle-right me-2"></i> Furniture</a>
+                            <a href="#"><i class="fas fa-angle-right me-2"></i> Electronics</a>
+                            <a href="#"><i class="fas fa-angle-right me-2"></i> Other products</a>
+                            <a href="#"><i class="fas fa-angle-right me-2"></i> Testimonies</a>
+                            <a href="#"><i class="fas fa-angle-right me-2"></i> Contact us</a>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-6 col-xl-3">
+                        <div class="footer-item">
+                            <h4 class="text-white mb-4">Developed by</h4>
+                            
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-6 col-xl-3">
+                        <div class="footer-item">
+                            <h4 class="text-white mb-4">Contact Info</h4>
+                            <div class="d-flex align-items-center">
+                                <i class="fas fa-map-marker-alt text-primary me-3"></i>
+                                <p class="text-white mb-0">Dar - Es - Salaam</p>
+                            </div>
+                            <div class="d-flex align-items-center">
+                                <i class="fas fa-envelope text-primary me-3"></i>
+                                <p class="text-white mb-0">eandrenterprise@gmail.com</p>
+                            </div>
+                            <div class="d-flex align-items-center">
+                                <i class="fa fa-phone-alt text-primary me-3"></i>
+                                <p class="text-white mb-0">(+255) 000 000 000</p>
+                            </div>
+                            <div class="d-flex align-items-center mb-4">
+                                <i class="fab fa-firefox-browser text-primary me-3"></i>
+                                <p class="text-white mb-0">eandrenterprise.com</p>
+                            </div>
+                            <div class="d-flex">
+                                <a class="btn btn-primary btn-sm-square rounded-circle me-3" href="#"><i class="fab fa-facebook-f text-white"></i></a>
+                                <a class="btn btn-primary btn-sm-square rounded-circle me-3" href="#"><i class="fab fa-twitter text-white"></i></a>
+                                <a class="btn btn-primary btn-sm-square rounded-circle me-3" href="#"><i class="fab fa-instagram text-white"></i></a>
+                                <a class="btn btn-primary btn-sm-square rounded-circle me-0" href="#"><i class="fab fa-linkedin-in text-white"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Footer End -->
+        
+        <!-- Copyright Start -->
+        <div class="container-fluid copyright py-4">
+            <div class="container">
+                <div class="row g-4 align-items-center">
+                    <div class="col-md-6 text-center text-md-start mb-md-0">
+                        <span class="text-body"><a href="#" class="border-bottom text-white"><i class="fas fa-copyright text-light me-2"></i>E&R Enterprises</a>, All right reserved.</span>
+                    </div>
+                    <div class="col-md-6 text-center text-md-end text-body">
+                        Designed By <a class="border-bottom text-white" href="https://rwazi.com">SyliverStarCompany</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Copyright End -->
+
+
+        <!-- Back to Top -->
+        <a href="#" class="btn btn-primary btn-lg-square rounded-circle back-to-top"><i class="fa fa-arrow-up"></i></a>   
+            
+        
+          <!-- Login modal -->
+        <div id="myModal" class="modal fade"  role="dialog" >
+    <div class="modal-dialog modal-lg" style="width:375px;">
+    
+      <!-- Modal content-->
+      <div class="modal-content wrapper" style="">
+        <div class="modal-body">
+        <div class="wrapper">
+        <div class="logo">
+            <img src="https://www.freepnglogos.com/uploads/twitter-logo-png/twitter-bird-symbols-png-logo-0.png" alt="">
+        </div>
+        <div class="text-center mt-4 name">
+            E&R Enterprises
+        </div>
+        <form class="p-3 mt-3" id="login_form">
+            <div id="fdbk"></div>
+            <div class="form-field d-flex align-items-center">
+                <span class="far fa-user"></span>
+                <input type="text" name="userName" id="username" placeholder="Username" required="required">
+            </div>
+            <div class="form-field d-flex align-items-center">
+                <span class="fas fa-key"></span>
+                <input type="password" name="password" id="pwd" placeholder="Password" required="required">
+            </div>
+            <button class="btn mt-3" onClick="checkLogin()" type="submit">Login</button>
+            <button type="button" class="btn2 mt-1" data-bs-dismiss="modal" aria-label="Close">close</button>
+        </form>
+    </div>
+        </div>
+      </div>
+      
+    </div>
+</div>
+        
+        <!-- JavaScript Libraries -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="public/web/lib/wow/wow.min.js"></script>
+        <script src="public/web/lib/easing/easing.min.js"></script>
+        <script src="public/web/lib/waypoints/waypoints.min.js"></script>
+        <script src="public/web/lib/counterup/counterup.min.js"></script>
+        <script src="public/web/lib/lightbox/js/lightbox.min.js"></script>
+        <script src="public/web/lib/owlcarousel/owl.carousel.min.js"></script>
+        
+
+        <!-- Template Javascript -->
+        <script src="<?php echo base_url('public/web/js/main.js')?>"></script>
+    </body>
+
+</html>
+
+<script type="text/javascript">
+    function login(){
+        $('#myModal').modal('show');
+    }
+
+    $('#login_form').submit(function (e) {
+        e.preventDefault();
+        $('#fdbk').html('<span class="fa fa-spinner fa-spin"></span> ...');
+    var dta = $('#login_form').serialize(); 
+                
+        $.ajax({
+            type: "POST",
+            url: "<?php echo base_url() . 'login_details'; ?>",
+            data: dta,
+            success: function (res) {
+                if(res==200){
+                    $('#fdbk').html("<div class='alert alert-success'><span class='fa fa-tick'></span> LoggedIn Successfuly</div>");
+                    window.location.replace('<?php echo base_url('dashboard'); ?>');
+                }else{
+          $('#fdbk').html(res);
+                }
+              
+            },
+            error: function () {
+                $('#fdbk').html('<div class="alert alert-warning">Failed to login.!!</div>');
+            }
+        });
+    });
+</script>
---
+++ app/Views/templates/header.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <title>E&R Enterprises</title>
+        <meta content="width=device-width, initial-scale=1.0" name="viewport">
+        <meta content="" name="keywords">
+        <meta content="" name="description">
+
+        <!-- Google Web Fonts -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet"> 
+
+        <!-- Icon Font Stylesheet -->
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"/>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+        <!-- Libraries Stylesheet -->
+        <link rel="stylesheet" href="public/web/lib/animate/animate.min.css"/>
+        <link href="public/web/lib/lightbox/css/lightbox.min.css" rel="stylesheet">
+        <link href="public/web/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+
+
+        <!-- Customized Bootstrap Stylesheet -->
+        <link href="public/web/css/bootstrap.min.css" rel="stylesheet">
+
+        <!-- Template Stylesheet -->
+        <link href="public/web/css/style.css" rel="stylesheet">
+        <link href="public/web/css/login.css" rel="stylesheet">
+    </head>
+
+    <body>
+        <!-- Spinner Start -->
+        <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+            <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
+                <span class="sr-only">Loading...</span>
+            </div>
+        </div>
+        <!-- Spinner End -->
+
+        <!-- Topbar Start -->
+        <div class="container-fluid topbar bg-light px-5 d-none d-lg-block">
+            <div class="row gx-0 align-items-center">
+                <div class="col-lg-8 text-center text-lg-start mb-2 mb-lg-0">
+                    <div class="d-flex flex-wrap">
+                        <a href="tel:+01234567890" class="text-muted small me-4"><i class="fas fa-phone-alt text-primary me-2"></i>(+255)000 000 000</a>
+                        <a href="mailto:eandrenterprise@gmail.com" class="text-muted small me-0"><i class="fas fa-envelope text-primary me-2"></i>eandrenterprise@gmail.com</a>
+                    </div>
+                </div>
+                <div class="col-lg-4 text-center text-lg-end">
+                    <div class="d-inline-flex align-items-center" style="height: 45px;">
+                        <a href="#" onclick="login()"><small class="me-3 text-dark"><i class="fa fa-sign-in-alt text-primary me-2"></i>Login</small></a>
+                    </div>
+                </div>
+                
+            </div>
+            
+        </div>
+        <!-- Topbar End -->
+       
+        <!-- Navbar & Hero Start -->
+        <div class="container-fluid position-relative p-0">
+            <nav class="navbar navbar-expand-lg navbar-light px-4 px-lg-5 py-3 py-lg-0">
+                <a href="" class="navbar-brand p-0">
+                    <h1 class="text-primary"><img src="public/web/img/logo.png" alt="Logo"></h1>
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                    <span class="fa fa-bars"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                    <div class="navbar-nav ms-auto py-0">
+                        
+                        <a href="index.html" class="nav-item nav-link active">Home</a>
+                        <div class="nav-item dropdown">
+                            <a href="#" class="nav-link" data-bs-toggle="dropdown">
+                                <span class="dropdown-toggle">Furnitures</span>
+                            </a>
+                            <div class="dropdown-menu m-0">
+                                <a href="feature.html" class="dropdown-item">Outdoor furniture</a>
+                                <a href="feature.html" class="dropdown-item">Home furniture</a>
+                                <a href="team.html" class="dropdown-item">School furniture</a>
+                                <a href="testimonial.html" class="dropdown-item">Recreation furniture</a>
+                                <a href="offer.html" class="dropdown-item">Office furniture</a>
+                                <a href="FAQ.html" class="dropdown-item">Play equipment (children)/Recreation centers</a>
+                                <a href="404.html" class="dropdown-item">Hall furniture</a>
+                                <a href="404.html" class="dropdown-item">Meeting room furniture</a>
+                            </div>
+                        </div>
+                        <a href="about.html" class="nav-item nav-link">Electronics</a>
+                        <a href="service.html" class="nav-item nav-link">Other Products</a>
+                        <a href="contact.html" class="nav-item nav-link">Testimonies</a>
+                        <a href="contact.html" class="nav-item nav-link">FAQS</a>
+                        <a href="contact.html" class="nav-item nav-link">Contact Us</a>
+                    </div>
+                    <a href="#" class="btn btn-primary rounded-pill py-2 px-4 my-3 my-lg-0 flex-shrink-0">Get Started</a>
+                </div>
+            </nav>
