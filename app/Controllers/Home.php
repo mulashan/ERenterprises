a40442586a5ec2a6df4b3d6e40886e4f9b8d6d40
Syliverius Syliakus Aporinary <syliveriusakus@gmail.com>
2024-12-27 18:43:19 +0300

    Introduction of sql to website page

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,11 +2,16 @@
 
 namespace App\Controllers;
 
+// use CodeIgnitor\Controller;
+use App\Models\WebModel;
+
 class Home extends BaseController
 {
-    public function index()
-    {
-        return view('templates/header')
+    public function index(){
+        $company = new WebModel();
+        $details = $company->company_info();
+        $data['company_details'] = $details;
+        return view('templates/header',$data)
                 .view('templates/index')
                 .view('templates/footer');
     }
--- app/Controllers/QueryController.php
+++ app/Controllers/QueryController.php
@@ -12,7 +12,13 @@ class QueryController extends BaseController
      ];
 
      $queries = [
-     "",
+     "ALTER TABLE `company_tbl` ADD `about_us_sms` VARCHAR(2000) NOT NULL AFTER `logo`; ",
+     "UPDATE `company_tbl` SET `about_us_sms` = '<p class='mb-4>'Based in Dar es Salaam, Tanzania, E&R Enterprise we are a trusted business with over six years of experience in sourcing furniture and electronics from China. We specialize in managing the entire process—receiving customer orders, purchasing, transporting, and distributing high-quality products across East Africa.</p>
+                    <p>Our mission is to connect you with the world’s finest styles and innovations. The Chinese furniture market offers a rich array of designs, from classic to modern, crafted from materials like solid wood and metal, meeting the diverse preferences of global buyers. Meanwhile, China’s electronics market provides high-quality products ranging from home appliances and solar panels to audio equipment and smart devices.</p>
+                    <p>We collaborate with leading manufacturers in China to ensure that our customers receive premium products at competitive prices. Let us simplify your shopping experience by handling every step of the process with professionalism and reliability.</p>' WHERE `company_tbl`.`id` = 1;",
+                    "ALTER TABLE `company_tbl` ADD `company_start_year` INT NOT NULL AFTER `about_us_sms`; ",
+                    "UPDATE `company_tbl` SET `company_start_year` = '2018' WHERE `company_tbl`.`id` = 1; ",
+                    
     
     ];
 
